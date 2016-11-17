72644fbd63e7343425b63cf4783c5a4c4605d8d5
krissasantos <krissa.santos31@gmail.com>
2016-11-16 23:24:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much.";
+// Props allow us to pass values into our components
+//example of component: <MovieCard /> or <TableRow />
+
+
+// adding props as attribute to your component is 
+//like adding a key to your data set in an href
+
+//getDefaultProps is a thing
+//use ReactcreateClass({
+//   getDefaultProps()
+//     return {
+//       poster: 'http:/nknwkdlwa'
+
+//   }
+//   render()
+//   }
+
+// })
